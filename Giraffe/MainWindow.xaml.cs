8e630cbc284df73eada1fe2d37a51d1df7720554
manveti <manveti@users.noreply.github.com>
2017-12-19 05:30:45 -0800

    Add support for status text and fullscreen media

--- Giraffe/MainWindow.xaml.cs
+++ Giraffe/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace Giraffe {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window, IDisplayHandler {
+        WindowState previousWindowState = WindowState.Normal;
+
         public MainWindow() {
             InitializeComponent();
             this.browser.DisplayHandler = this;
@@ -42,9 +44,47 @@ namespace Giraffe {
 
         public bool OnConsoleMessage(IWebBrowser sender, ConsoleMessageEventArgs e) { return false; }
         public void OnFaviconUrlChange(IWebBrowser sender, IBrowser b, IList<String> urls) { }
-        public void OnFullscreenModeChange(IWebBrowser sender, IBrowser b, bool fullscreen) { }
-        public void OnStatusMessage(IWebBrowser sender, StatusMessageEventArgs e) { }
-        public void OnTitleChanged(IWebBrowser sender, TitleChangedEventArgs e) { }
+
+        public void OnFullscreenModeChange(IWebBrowser sender, IBrowser b, bool fullscreen) {
+            this.Dispatcher.Invoke(new Action(() => {
+                if (fullscreen) { this.enterFullscreen(); }
+                else { this.exitFullscreen(); }
+            }));
+        }
+
+        public void OnStatusMessage(IWebBrowser sender, StatusMessageEventArgs e) {
+            this.statusBox.Dispatcher.Invoke(new Action(() => { this.statusBox.Text = e.Value; }));
+        }
+
+        public void OnTitleChanged(IWebBrowser sender, TitleChangedEventArgs e) {
+            this.Dispatcher.Invoke(new Action(() => {
+                if ((e.Title != null) && (e.Title.Length > 0)) {
+                    this.Title = e.Title + " - Giraffe";
+                }
+                else {
+                    this.Title = "Giraffe";
+                }
+            }));
+        }
+
         public bool OnTooltipChanged(IWebBrowser sender, String text) { return false; }
+
+
+        // helper functions
+        public void enterFullscreen() {
+            this.browserBorder.Child = null;
+            this.Content = this.browser;
+            this.previousWindowState = this.WindowState;
+            this.WindowState = WindowState.Normal;
+            this.WindowStyle = WindowStyle.None;
+            this.WindowState = WindowState.Maximized;
+        }
+
+        public void exitFullscreen() {
+            this.Content = this.mainGrid;
+            this.browserBorder.Child = this.browser;
+            this.WindowStyle = WindowStyle.SingleBorderWindow;
+            this.WindowState = this.previousWindowState;
+        }
     }
 }
